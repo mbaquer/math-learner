d087bbe0fa832cb9a2ede2370ba759448679a7b4
Mohammad <Mohammad@MacBook-Pro-99.local>
2025-03-05 15:32:10 -0500

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
-import type { Route } from "./+types/home";
+import React from "react";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Math Learner" },
   ];
 }
 
